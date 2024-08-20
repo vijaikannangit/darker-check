54aa3036b1aa9093a6906630762f7940a9b1ca54
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-20 12:40:56 +0530

    Update darker-sample.py

--- darker-sample.py
+++ darker-sample.py
@@ -6,7 +6,6 @@ import date
 def hello_world():
     print("Boo Hello sir, darker sample test4 and test4")
     print("Boo Hello sir, darker sample test5 and test5")
-    print("Boo Hello sir, darker sample test6 and test6")
 
 hello_world()
 print("Just 1")
