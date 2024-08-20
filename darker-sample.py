6ec49af047f2904378ba1589803806f5c5288dc2
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-20 15:57:04 +0530

    Update darker-sample.py

--- darker-sample.py
+++ darker-sample.py
@@ -1,11 +1,10 @@
 # Validate python code
-import math 
 import datetime
 import date
 import math
 
 def hello_world():
-    print("Boo Hello sir, darker sample test4 and test4")
+    print("Boo Hello oooo sir, darker sample test4 and test4")
     print("Boo Hello sir, darker sample test5 and test5")
 
 hello_world()
